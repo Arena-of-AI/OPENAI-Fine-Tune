0cc8fd6f63cc4f1129963311a761927e62acf55a
Arena-of-AI <132893291+Arena-of-AI@users.noreply.github.com>
2023-05-08 17:37:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+import re
+import streamlit as st
+
+# 正则表达式模式，用于验证OpenAI API Key格式
+API_KEY_REGEX = r"^[a-z0-9]{32}$"
+
+# 创建文本框组件
+api_key_input = st.text_input("Enter your OpenAI API Key:")
+
+# 验证API Key格式
+if api_key_input:
+    if not re.match(API_KEY_REGEX, api_key_input):
+        st.error("Invalid OpenAI API Key format!")
+    else:
+        st.success("OpenAI API Key is valid!")
 
