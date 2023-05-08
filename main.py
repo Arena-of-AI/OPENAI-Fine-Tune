6ff3a2990b6cc98b07d1aa5ce9fd8d51525c347d
Arena-of-AI <132893291+Arena-of-AI@users.noreply.github.com>
2023-05-08 19:18:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,22 +13,22 @@ except Exception as e:
     st.error(f"無法連接OpenAI API，請檢查API Key是否正確：{e}")
     st.stop()
 
-# 上傳檔案
-uploaded_file = st.file_uploader("上傳Excel檔案", type=["xlsx"])
+# 讓使用者上傳自己的training data（使用excel格式）
+uploaded_file = st.file_uploader("Upload Excel File", type=["xlsx"])
 
-# 轉換格式
 if uploaded_file is not None:
-    st.text("格式轉換中...")
     try:
-        # 讀取Excel檔案
+        # 讀取excel檔案
         df = pd.read_excel(uploaded_file)
-
-        # 將每一行轉換成JSONL格式
-        with jsonlines.open("training_data.jsonl", mode="w") as writer:
-            for index, row in df.iterrows():
-                writer.write({"text": row["text"], "label": row["label"]})
-        
-        st.success("檔案轉換已完成")
+        # 轉換成jsonl格式
+        jsonl = openai.FineTune.prepare_data(df=df)
+        # 顯示檔案轉換已完成
+        st.success('File conversion completed!')
+        # 下載jsonl檔案
+        href = f'<a href="data:application/jsonl;base64,{jsonl}">Download jsonl file</a>'
+        st.markdown(href, unsafe_allow_html=True)
         st.download_button(label="Download jsonl", data=jsonl, file_name="training_data.jsonl", mime="application/jsonl")
+    except errors.AuthenticationError:
+        st.error("Invalid OpenAI API key")
     except Exception as e:
-        st.error(f"轉換檔案時發生錯誤：{e}")
+        st.error(f"Error: {e}")
