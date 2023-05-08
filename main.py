7d18bd7e09539d0ebfe47bbf592f33b88bd8b8db
Arena-of-AI <132893291+Arena-of-AI@users.noreply.github.com>
2023-05-08 18:52:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,54 +1,27 @@
-import re
 import streamlit as st
+import pandas as pd
+import json
 
-# 正则表达式模式，用于验证OpenAI API Key格式
-API_KEY_REGEX = r"^[a-z0-9]{32}$"
+def excel_to_jsonl(file):
+    # 讀取Excel檔案，並將數據轉換為DataFrame格式
+    data = pd.read_excel(file)
 
-# 创建文本框组件
-api_key_input = st.text_input("Enter your OpenAI API Key:")
+    # 將DataFrame轉換為JSON格式
+    json_data = json.loads(data.to_json(orient='records'))
 
-# 验证API Key格式
-if api_key_input:
-    if not re.match(API_KEY_REGEX, api_key_input):
-        st.error("Invalid OpenAI API Key format!")
-    else:
-        st.success("OpenAI API Key is valid!")
+    # 將JSON格式寫入JSONL文件
+    with open('data.jsonl', 'w', encoding='utf-8') as f:
+        for record in json_data:
+            f.write(json.dumps(record, ensure_ascii=False) + '\n')
 
-# 讀取現有的模型列表
-def get_model_list():
-    model_list = openai.Model.list()
-    return [model.id for model in model_list["data"]]
+# 主要程式碼
+st.title('Training Data Converter')
 
-# 建立自訂模型
-def create_custom_model(model_name):
-    model = openai.Model.create(
-        id=model_name,
-        language="en",
-        training_data=[],
-        max_documents=10000,
-        n_epochs=1,
-        model="text-davinci-002",
-    )
-    return model.id
+# 讓使用者上傳Excel檔案
+uploaded_file = st.file_uploader('Upload Training Data', type=['xlsx'])
 
-# Streamlit App
-def app():
-    st.title("OpenAI Fine Tune Training")
-    st.write("")
-
-    # 使用者輸入OpenAI API Key
-    api_key = st.text_input("Enter OpenAI API Key")
-
-    # 驗證OpenAI API Key格式是否正確
-    if validate_api_key(api_key):
-        st.write("API Key format is valid")
-        openai.api_key = api_key
-    else:
-        st.write("Invalid API Key format")
-
-    # 確認API Key
-    if st.button("Confirm API Key"):
-        if openai.api_key != "":
-            st.write("API Key confirmed")
-        else:
-            st.write("Please enter a valid API Key")
+# 如果有上傳檔案，則進行格式轉換
+if uploaded_file:
+    st.write('Converting file...')
+    excel_to_jsonl(uploaded_file)
+    st.write('File conversion completed!')
